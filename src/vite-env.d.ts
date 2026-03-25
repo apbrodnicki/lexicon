123558fbd13d4e3b4b1c1eda770135ae3df0b33b
Alex Brodnicki <apbrodnicki@gmail.com>
2026-03-24 23:37:29 -0500

    Add wrangler file.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,14 +1,1 @@
 /// <reference types="vite/client" />
-
-interface ViteTypeOptions {
-	strictImportMetaEnv: unknown;
-}
-
-interface ImportMetaEnv {
-	readonly VITE_KEY_1: string; // import.meta.env.VITE_KEY_1
-	readonly VITE_KEY_2: string; // import.meta.env.VITE_KEY_2
-}
-
-interface ImportMeta {
-	readonly env: ImportMetaEnv;
-}

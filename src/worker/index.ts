39ca9b97fa3f49e08eeb2132d0118025d2ebf836
Alex Brodnicki <apbrodnicki@gmail.com>
2026-03-28 23:15:58 -0500

    Use environment variables and remove api key from public code entirely like it should've been all along.

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,11 +1,13 @@
 import { Hono } from 'hono';
+import { env } from 'hono/adapter';
 
 const app = new Hono<{ Bindings: Env; }>();
 
 app.get('/api/fetchWord', async (c) => {
 	const query = c.req.query('query');
+	const { DICTIONARY_TOKEN } = env<{ DICTIONARY_TOKEN: string; }>(c);
 
-	const response = await fetch(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${query}?key=62ae6585-05f1-46c3-8cda-757eca0f6e87`);
+	const response = await fetch(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${query}?key=${DICTIONARY_TOKEN}`);
 	const data = await response.json();
 
 	return c.json(data);

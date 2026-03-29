441633782df4c9e67ce41bd8ff973e6e87190762
Alex Brodnicki <apbrodnicki@gmail.com>
2026-03-28 23:44:18 -0500

    Simplify env call.

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,4 +1,3 @@
-import { env } from 'cloudflare:workers';
 import { Hono } from 'hono';
 
 const app = new Hono<{ Bindings: Env; }>();
@@ -6,7 +5,7 @@ const app = new Hono<{ Bindings: Env; }>();
 app.get('/api/fetchWord', async (c) => {
 	const query = c.req.query('query');
 
-	const response = await fetch(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${query}?key=${env.DICTIONARY_TOKEN}`);
+	const response = await fetch(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${query}?key=${c.env.DICTIONARY_TOKEN}`);
 	const data = await response.json();
 
 	return c.json(data);

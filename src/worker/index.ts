fbb4e7019cd87a81815ab5a7bb4d1885fa127e6b
Alex Brodnicki <apbrodnicki@gmail.com>
2026-03-30 13:32:56 -0500

    Comment route protection temporarily to continue development.

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,18 +1,18 @@
 import { Hono } from 'hono';
 import { HTTPException } from 'hono/http-exception';
-import { jwt } from 'hono/jwt';
+// import { jwt } from 'hono/jwt';
 import auth from './routes/auth';
 import dictionary from './routes/dictionary';
 
 const app = new Hono<{ Bindings: Env; }>();
 
-app.use('/api/*', async (c, next) => {
-	if (c.req.path.startsWith('/api/auth')) {
-		return next();
-	}
+// app.use('/api/*', async (c, next) => {
+// 	if (c.req.path.startsWith('/api/auth')) {
+// 		return next();
+// 	}
 
-	return jwt({ secret: c.env.JWT_SECRET, alg: 'HS256' })(c, next);
-});
+// 	return jwt({ secret: c.env.JWT_SECRET, alg: 'HS256' })(c, next);
+// });
 
 app.route('/api/dictionary', dictionary);
 app.route('/api/auth', auth);

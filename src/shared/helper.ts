a1104a01dead97ff07d632b238c5dd41877d0534
Alex Brodnicki <apbrodnicki@gmail.com>
2026-03-31 16:50:43 -0500

    Move some code around.

--- src/shared/helper.ts
+++ src/shared/helper.ts
@@ -1,36 +1,3 @@
-import type { GenericWord } from './models/genericModels';
-import type { PartsOfSpeech, Word } from './models/models';
-
-export const filterGenericWordData = (word: GenericWord): Word => {
-	return {
-		id: word.meta.id,
-		stems: word.meta.stems,
-		offensive: word.meta.offensive,
-		speechPart: word.fl as keyof PartsOfSpeech,
-		definitions: word.shortdef,
-	};
-};
-
-export const filterGenericWords = (genericWords: GenericWord[]): Word[] => {
-	const words: Word[] = [];
-
-	for (const currentGenericWord of genericWords) {
-		// Skip words with no definitions (they're just alternate spellings)
-		if (currentGenericWord.shortdef.length < 1) {
-			continue;
-		}
-
-		words.push(filterGenericWordData(currentGenericWord));
-	}
-
-	return words;
-};
-
-/**
- * Capitalizes the first letter in a string
- * @param word: string
- * @returns string
- */
 export const capitalizeFirstLetter = (word: string): string => {
 	return word.charAt(0).toUpperCase() + word.slice(1);
 };
---
+++ src/worker/helper/filterApiData.ts
@@ -0,0 +1,27 @@
+import type { GenericWord } from '@shared/models/genericModels';
+import type { PartsOfSpeech, Word } from '@shared/models/models';
+
+export const filterGenericWordData = (word: GenericWord): Word => {
+	return {
+		id: word.meta.id,
+		stems: word.meta.stems,
+		offensive: word.meta.offensive,
+		speechPart: word.fl as keyof PartsOfSpeech,
+		definitions: word.shortdef,
+	};
+};
+
+export const filterGenericWords = (genericWords: GenericWord[]): Word[] => {
+	const words: Word[] = [];
+
+	for (const currentGenericWord of genericWords) {
+		// Skip words with no definitions (they're just alternate spellings)
+		if (currentGenericWord.shortdef.length < 1) {
+			continue;
+		}
+
+		words.push(filterGenericWordData(currentGenericWord));
+	}
+
+	return words;
+};

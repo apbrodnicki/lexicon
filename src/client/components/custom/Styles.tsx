5841c5972e3039fc036355079f25f8a2f349b6e9
Alex Brodnicki <apbrodnicki@gmail.com>
2026-04-03 18:25:54 -0500

    Add permanent box shadow for mobile clarity, could be better I think.

--- src/client/components/custom/Styles.tsx
+++ src/client/components/custom/Styles.tsx
@@ -7,12 +7,26 @@ export const StyledListItemButton = styled(ListItemButton)(() => ({
 				0px 3px 4px 0px rgba(0, 0, 0, 0.14),
 				0px 1px 8px 0px rgba(0, 0, 0, 0.12)
 			`
+	},
+	'@media (max-width: 600px)': {
+		boxShadow: `
+				0px 3px 3px -2px rgba(0, 0, 0, 0.2),
+				0px 3px 4px 0px rgba(0, 0, 0, 0.14),
+				0px 1px 8px 0px rgba(0, 0, 0, 0.12)
+			`
 	}
 }));
 
 export const StyledIconButton = styled(IconButton)(() => ({
 	borderRadius: 0,
 	'&:hover': {
+		boxShadow: `
+				0px 3px 3px -2px rgba(0, 0, 0, 0.2),
+				0px 3px 4px 0px rgba(0, 0, 0, 0.14),
+				0px 1px 8px 0px rgba(0, 0, 0, 0.12)
+			`
+	},
+	'@media (max-width: 600px)': {
 		boxShadow: `
 				0px 3px 3px -2px rgba(0, 0, 0, 0.2),
 				0px 3px 4px 0px rgba(0, 0, 0, 0.14),

9b22f28e9e674074d18bdf536958ed3ff25ce2cb
Alex Brodnicki <apbrodnicki@gmail.com>
2026-04-04 01:41:23 -0500

    remove outdated sql

--- src/worker/schema.sql
+++ src/worker/schema.sql
@@ -2,10 +2,3 @@
 	Deploy command:
 	npx wrangler d1 execute lexicon-db --remote --file=src/worker/schema.sql
 */
-
-DROP TABLE IF EXISTS Words;
-CREATE TABLE IF NOT EXISTS Words (
-	WordId INTEGER PRIMARY KEY,
-	Word TEXT
-);
-INSERT INTO Words (WordId, Word) VALUES (1, 'elegy')
